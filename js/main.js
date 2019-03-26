f6907643d7a491d8c960c03cf53c1a94866b2fb8
Monsterup <yudhamanage@gmail.com>
2019-03-26 13:39:33 +0700

    Quiz

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
-if (navigator.serviceWorker){
+if ('serviceWorker' in navigator){
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(
+        navigator.serviceWorker.register('/serviceworker.js').then(
             function (reg) {
-                // console.log('SW registration success, scope :',reg.scope);
-                return navigator.serviceWorker.ready;
-            }).then(function(reg){
-                document.getElementById('req-sync')
-                .addEventListener('click', function(){
-                    reg.sync.register('image-fetch').then(function(){
-                        console.log('sync-registered');
-                    }).catch(function(err){
-                        console.log('unable to fetch image. Error : ', err);
-                    });
-                });
+                // registerasi service worker berhasil
+                console.log('SW registration success, scope :',reg.scope);
             }, function (err) {
-                console.log('SW registration failed : ', err);
-            });
+                // reg failed
+                console.log('SW registration faild : ', err);
+            }
+        )
     })
 }
